0360622075e8ab67aa8c47b718bf4502c916abce
jgaonakm <130958905+jgaonakm@users.noreply.github.com>
2025-07-17 18:09:49 -0600

    Add CORS config for testing

--- accounts/Program.cs
+++ accounts/Program.cs
@@ -4,18 +4,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 // Add services to the container.
+
+services.AddCors(options =>
+{
+        options.AddPolicy(name: "AllowedOrigins",
+            policy =>
+            {
+                    policy
+                    .WithOrigins("https://localhost:8443") // For local dev only!!
+                    .WithOrigins(
+                        "https://*.zuplo.dev", // For demo environment in Zuplo
+                        "https://*.akamai-apl.net") // For APIs hosted using Akamai AppPlatform
+                        .SetIsOriginAllowedToAllowWildcardSubdomains();
+            });
+});
 services.AddControllers()
         .AddXmlDataContractSerializerFormatters();
 
 services.AddSingleton<ICustomerService, CustomerService>();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseCors("AllowedOrigins");
 
 
 app.MapGet("/", () => "Jericalla Mobile API - Accounts");
